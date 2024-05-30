576e563dacf052c874fa2e4a81b9da69c49c638c
Frank Lee <flee63@gatech.edu>
2024-05-29 18:36:10 -0700

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
 import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
-
-const client = generateClient<Schema>();
+import Inner from "./inner";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
-
   return (
     <Authenticator>
-      <main>
-        <h1>My todos</h1>
-        <button onClick={createTodo}>+ new</button>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-              {todo.content}
-            </li>
-          ))}
-        </ul>
-        <div>
-          🥳 App successfully hosted. Try creating a new todo.
-          <br />
-          <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-            Review next step of this tutorial.
-          </a>
-        </div>
-      </main>
+      {({ signOut, user }) => <Inner user={user} />}
     </Authenticator>
   );
 }
